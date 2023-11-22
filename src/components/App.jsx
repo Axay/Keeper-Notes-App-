228cc3a56bb76be20a7124e7c0d95654cc308eb9
Akshay Shailesh <akshayshailesh@gmail.com>
2023-11-22 20:02:14 +0530

    Database added, UI updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from "axios";
+
+const backendServerURL =
+	"https://keeperapi-oasa.onrender.com" || "http://localhost:5000";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+	const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
+	//   function addNote(newNote) {
+	//     setNotes((prevNotes) => {
+	//       return [...prevNotes, newNote];
+	//     });
+	//   }
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
-  }
+	//   function deleteNote(id) {
+	//     setNotes((prevNotes) => {
+	//       return prevNotes.filter((noteItem, index) => {
+	//         return index !== id;
+	//       });
+	//     });
+	//   }
+	useEffect(() => {
+		axios
+			.get(backendServerURL + "/api/getAll")
+			.then((res) => setNotes(res.data));
+	}, []);
 
-  return (
-    <div>
-      <Header />
-      <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
-            onDelete={deleteNote}
-          />
-        );
-      })}
-      <Footer />
-    </div>
-  );
+	function addNote(newNote) {
+		if (newNote.title || newNote.content) {
+			axios
+				.post(backendServerURL + "/api/addNew", newNote)
+				.then((res) => setNotes(res.data));
+		}
+	}
+
+	const deleteNote = (id) => {
+		axios
+			.post(backendServerURL + "/api/delete", { id })
+			.then((res) => setNotes(res.data));
+	};
+
+	return (
+		<div>
+			<Header />
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem, index) => {
+				return (
+					<Note
+						key={index}
+						id={noteItem._id}
+						title={noteItem.title}
+						content={noteItem.content}
+						onDelete={deleteNote}
+					/>
+				);
+			})}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
